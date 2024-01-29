79070f57acf10f0c8005eeba88c15c27d7e3af69
Deidre Hunt <129712664+ddy623@users.noreply.github.com>
2024-01-28 19:11:49 -0500

    update

--- Data/movies.sql
+++ Data/movies.sql
@@ -1,7 +1,12 @@
+DROP SCHEMA IF EXISTS `movies`;
+CREATE SCHEMA IF NOT EXISTS `movies` DEFAULT CHARACTER SET utf8;
+USE `movies`;
+
+
 DROP TABLE IF EXISTS `genres`;
-DROP TABLE IF EXISTS `title.basics`;
-DROP TABLE IF EXISTS `title.genres`;
-DROP TABLE IF EXISTS `title.ratings`;
+DROP TABLE IF EXISTS `title_basics`;
+DROP TABLE IF EXISTS `title_genres`;
+DROP TABLE IF EXISTS `title_ratings`;
 
 CREATE TABLE `genres`  (
   `genre_id` int NOT NULL,
@@ -9,7 +14,7 @@ CREATE TABLE `genres`  (
   PRIMARY KEY (`genre_id` DESC)
 );
 
-CREATE TABLE `title.basics`  (
+CREATE TABLE `title_basics`  (
   `tconst` varchar(15) NOT NULL,
   `primary_title` varchar(255) NULL,
   `start_year` Int NULL,
@@ -17,12 +22,12 @@ CREATE TABLE `title.basics`  (
   PRIMARY KEY (`tconst` DESC)
 );
 
-CREATE TABLE `title.genres`  (
+CREATE TABLE `title_genres`  (
   `genre_id` Int NOT NULL,
   `tconst` char(15) NULL
 );
 
-CREATE TABLE `title.ratings`  (
+CREATE TABLE `title_ratings`  (
   `tconst` varchar(55) NOT NULL,
   `average_rating` varchar(45) NULL,
   `number_of_votes` int NULL,
